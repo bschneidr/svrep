c6667988240491783116c7521a47abe35ab677b3
Ben Schneider <benjamin.julius.schneider@gmail.com>
2022-07-05 00:42:04 -0400

    Ref #7. Add `by` argument to `summarize_rep_weights()`.

--- R/summarize_rep_weights.R
+++ R/summarize_rep_weights.R
@@ -9,6 +9,7 @@
 #' \cr
 #' Use \code{type = "both"} for a list containing both summaries,
 #' with the list containing the names \code{"overall"} and \code{"both"}.
+#' @param by (Optional) A character vector with the names of variables used to group the summaries.
 #'
 #' @return If \code{type = "both"} (the default), the result is a list of data frames
 #' with names \code{"overall"} and \code{"specific"}. If \code{type = "overall"}, the result is
@@ -69,6 +70,14 @@
 #'
 #' summarize_rep_weights(rep_design, type = "both")
 #'
+#' # Summarize replicate weights by grouping variables
+#'
+#' summarize_rep_weights(ue_adjusted_design, type = 'overall',
+#'                       by = c("response_status"))
+#'
+#' summarize_rep_weights(ue_adjusted_design, type = 'overall',
+#'                       by = c("stype", "response_status"))
+#'
 #' # Compare replicate weights
 #'
 #' rep_wt_summaries <- lapply(list('original' = rep_design,
@@ -77,7 +86,7 @@
 #'                            type = "overall")
 #' print(rep_wt_summaries)
 #'
-summarize_rep_weights <- function(rep_design, type = 'both') {
+summarize_rep_weights <- function(rep_design, type = 'both', by) {
 
   if (!'svyrep.design' %in% class(rep_design)) {
     stop("`rep_design` must be a replicate design object, with class 'svyrep.design'.")
@@ -93,85 +102,226 @@ summarize_rep_weights <- function(rep_design, type = 'both') {
     }
   }
 
+  if (missing(by) || is.null(by)) {
+    by <- NULL
+  } else {
+    if (!is.character(by)) {
+      stop("`by` must be a character vector with at least one column name from `design`.")
+    }
+    if (!all(by %in% colnames(rep_design[['variables']]))) {
+      missing_variables <- setdiff(by, colnames(rep_design[['variables']]))
+      error_msg <- sprintf("The following `by` variables are missing from the data: %s",
+                           paste(missing_variables, collapse = ", "))
+      stop(error_msg)
+    }
+  }
+
   # Make sure replicate weights
   # are the "combined" weights
   # (i.e. incorporating base sampling weights)
-  if (rep_design$combined.weights) {
-    rep_wts_matrix <- as.matrix(rep_design$repweights)
+  rep_wts_matrix <- weights(object = rep_design, type = 'analysis')
+
+  # If summaries should be grouped, divide data into groups, and obtain list of row indices for each group
+
+  if (!is.null(by)) {
+    is_grouped <- TRUE
+    ## Create factor whose levels are observed combinations of grouping variables
+    unique_values_factor <- interaction(rep_design$variables[,by],
+                                        sep = "_<>_", drop = TRUE)
+    ## Obtain a dataframe giving the unique values of the combinations of grouping variables
+    first_index_per_unique_combo <- !duplicated(unique_values_factor)
+    groups_df <- rep_design$variables[first_index_per_unique_combo, by, drop=FALSE]
+    ## Reorder rows of the dataframe to match the order of the levels of the unique values factor
+    desired_row_order <- unique_values_factor[first_index_per_unique_combo] |>
+      as.numeric() |> order()
+    groups_df <- groups_df[desired_row_order, , drop = FALSE]
+
+    ## Get row indices for each group
+    group_row_indices <- lapply(levels(unique_values_factor),
+                                function(factor_level) which(unique_values_factor == factor_level))
+    names(group_row_indices) <- levels(unique_values_factor)
   } else {
-    rep_wts_matrix <- apply(as.matrix(rep_design$repweights),
-                            MARGIN = 2, FUN = function(rep_wt_set) {
-                              rep_wt_set * rep_design$pweights
-                            })
+    is_grouped <- FALSE
+    group_row_indices <- list(seq_len(nrow(rep_design)))
+  }
+
+  if (nrow(rep_design) == 0) {
+    group_row_indices <- lapply(group_row_indices, FUN = function(x) 0L)
+  }
+
+  # Special handling if the replicate design has zero rows of data
+  if (nrow(rep_design) == 0) {
+
+    rep_wt_dims <- setNames(dim(rep_wts_matrix),
+                            nm = c("rows", "cols"))
+
+    ## Prepare overall summary
+    overall_summary <- data.frame(
+      'nrows' = rep_wt_dims['rows'],
+      'ncols' = rep_wt_dims['cols'],
+      'degf_svy_pkg' = NA_integer_,
+      'rank' = NA_integer_,
+      'avg_wgt_sum' = NA_real_,
+      'sd_wgt_sums' = NA_real_,
+      'min_rep_wgt' = NA_real_,
+      'max_rep_wgt' = NA_real_,
+      row.names = NULL
+    )
+
+    ## Prepare replicate-specific summaries
+    wt_set_summary <- c(N = 0,
+                        N_NONZERO = 0,
+                        SUM = NA_real_,
+                        MEAN = NA_real_,
+                        CV = NA_real_,
+                        MIN = NA_real_,
+                        MAX = NA_real_)
+
+    wt_set_summaries <- lapply(X = seq_len(rep_wt_dims['cols']), FUN = function(rep_column) {
+      wt_set_summary
+    }) |> do.call(what = rbind)
+
+    if (!is.null(colnames(rep_wts_matrix))) {
+      wt_set_summaries <- cbind(data.frame("Rep_Column" = colnames(rep_wts_matrix)),
+                                wt_set_summaries,
+                                row.names = NULL)
+    } else {
+      wt_set_summaries <- cbind(data.frame("Rep_Column" = seq_len(nrow(wt_set_summaries))),
+                                wt_set_summaries,
+                                row.names = NULL)
+    }
+
+    if (type == "overall") {
+      result <- overall_summary
+    }
+    if (type == "specific") {
+      result <- wt_set_summaries
+    }
+    if (type == "both") {
+      result <- list(
+        'overall' = overall_summary,
+        'specific' = wt_set_summaries
+      )
+    }
+    return(result)
   }
 
   # Summarize each set of replicate weights
-  wt_set_summaries <- apply(rep_wts_matrix,
-                            simplify = FALSE, MARGIN = 2,
-                            FUN = function(wt_set) {
+  wt_set_summaries_by_group <- lapply(
+    X = group_row_indices, FUN = function(row_indices) {
 
-                              SD <- stats::sd(wt_set)
-                              MEAN <- mean(wt_set)
-                              CV <- SD/MEAN
+      rep_wts_matrix <- rep_wts_matrix[row_indices,,drop=FALSE]
 
-                              c(N = nrow(rep_wts_matrix),
-                                N_NONZERO = sum(wt_set != 0),
-                                SUM = sum(wt_set),
-                                MEAN = MEAN,
-                                CV = CV,
-                                MIN = min(wt_set),
-                                MAX = max(wt_set))
-                            })
+      wt_set_summaries <- apply(rep_wts_matrix,
+                                simplify = FALSE, MARGIN = 2,
+                                FUN = function(wt_set) {
 
-  wt_set_summaries <- do.call(what = rbind,
-                              args = wt_set_summaries)
-  wt_set_summaries <- as.data.frame(wt_set_summaries)
+                                  SD <- stats::sd(wt_set)
+                                  MEAN <- mean(wt_set)
+                                  CV <- SD/abs(MEAN)
 
-  if (!is.null(colnames(rep_wts_matrix))) {
-    wt_set_summaries <- cbind(data.frame("Rep_Column" = colnames(rep_wts_matrix)),
-                              wt_set_summaries)
-  } else {
-    wt_set_summaries <- cbind(data.frame("Rep_Column" = seq_len(nrow(wt_set_summaries))),
-                              wt_set_summaries)
-  }
-  rownames(wt_set_summaries) <- NULL
+                                  c(N = nrow(rep_wts_matrix),
+                                    N_NONZERO = sum(wt_set != 0),
+                                    SUM = sum(wt_set),
+                                    MEAN = MEAN,
+                                    CV = CV,
+                                    MIN = min(wt_set),
+                                    MAX = max(wt_set))
+                                })
+
+      wt_set_summaries <- do.call(what = rbind,
+                                  args = wt_set_summaries)
+      wt_set_summaries <- as.data.frame(wt_set_summaries)
+
+      if (!is.null(colnames(rep_wts_matrix))) {
+        wt_set_summaries <- cbind(data.frame("Rep_Column" = colnames(rep_wts_matrix)),
+                                  wt_set_summaries,
+                                  row.names = NULL)
+      } else {
+        wt_set_summaries <- cbind(data.frame("Rep_Column" = seq_len(nrow(wt_set_summaries))),
+                                  wt_set_summaries,
+                                  row.names = NULL)
+      }
+      rownames(wt_set_summaries) <- NULL
+
+      return(wt_set_summaries)
+    }
+  )
 
   # Produce an overall summary
-  max_rep_wgt <- max(wt_set_summaries$MAX)
-  min_rep_wgt <- min(wt_set_summaries$MIN)
-  sd_wgt_sums <- stats::sd(wt_set_summaries$SUM)
-  avg_wgt_sum <- mean(wt_set_summaries$SUM)
+  if (type %in% c("overall", "both")) {
 
-  matrix_rank <- qr(x = rep_wts_matrix, tol = 1e-05)[['rank']]
+    overall_summaries_by_group <- lapply(
+      X = seq_along(group_row_indices), FUN = function(group_index) {
 
-  rep_wt_dims <- setNames(dim(rep_wts_matrix),
-                          nm = c("rows", "cols"))
+        wt_set_summaries <- wt_set_summaries_by_group[[group_index]]
+        rep_wts_matrix <- rep_wts_matrix[group_row_indices[[group_index]],,drop=FALSE]
 
-  degf_svy_pkg <- survey::degf(rep_design)
+        max_rep_wgt <- max(wt_set_summaries$MAX)
+        min_rep_wgt <- min(wt_set_summaries$MIN)
+        sd_wgt_sums <- stats::sd(wt_set_summaries$SUM)
+        avg_wgt_sum <- mean(wt_set_summaries$SUM)
 
-  summary_df <- data.frame(
-    'nrows' = rep_wt_dims['rows'],
-    'ncols' = rep_wt_dims['cols'],
-    'degf_svy_pkg' = degf_svy_pkg,
-    'rank' = matrix_rank,
-    'avg_wgt_sum' = avg_wgt_sum,
-    'sd_wgt_sums' = sd_wgt_sums,
-    'min_rep_wgt' = min_rep_wgt,
-    'max_rep_wgt' = max_rep_wgt,
-    row.names = NULL
-  )
+        matrix_rank <- qr(x = rep_wts_matrix, tol = 1e-05)[['rank']]
+
+        rep_wt_dims <- setNames(dim(rep_wts_matrix),
+                                nm = c("rows", "cols"))
+
+        degf_svy_pkg <- survey::degf(rep_design[group_row_indices[[group_index]],])
+
+        summary_df <- data.frame(
+          'nrows' = rep_wt_dims['rows'],
+          'ncols' = rep_wt_dims['cols'],
+          'degf_svy_pkg' = degf_svy_pkg,
+          'rank' = matrix_rank,
+          'avg_wgt_sum' = avg_wgt_sum,
+          'sd_wgt_sums' = sd_wgt_sums,
+          'min_rep_wgt' = min_rep_wgt,
+          'max_rep_wgt' = max_rep_wgt,
+          row.names = NULL
+        )
+        return(summary_df)
+      }
+    )
+  }
+
+  # Combine grouped results into single dataframe, if applicable
+
+  if (is_grouped) {
+    wt_set_summaries <- lapply(X = seq_along(group_row_indices), FUN = function(group_index) {
+      cbind(groups_df[group_index,,drop=FALSE],
+            wt_set_summaries_by_group[[group_index]],
+            row.names = NULL)
+    }) |> do.call(what = rbind)
+
+    if (type %in% c("overall", "both")) {
+      overall_summary <- lapply(X = seq_along(group_row_indices), FUN = function(group_index) {
+        cbind(groups_df[group_index,,drop=FALSE],
+              overall_summaries_by_group[[group_index]],
+              row.names = NULL)
+      }) |> do.call(what = rbind)
+    }
+  }
+
+  if (!is_grouped) {
+    wt_set_summaries <- wt_set_summaries_by_group[[1]]
+
+    if (type %in% c("overall", "both")) {
+      overall_summary <- overall_summaries_by_group[[1]]
+    }
+  }
 
   # Combine into a list
   # to return as output
   if (type == "overall") {
-    result <- summary_df
+    result <- overall_summary
   }
   if (type == "specific") {
     result <- wt_set_summaries
   }
   if (type == "both") {
     result <- list(
-      'overall' = summary_df,
+      'overall' = overall_summary,
       'specific' = wt_set_summaries
     )
   }
--- tests/testthat/test-summarize_rep_weights.R
+++ tests/testthat/test-summarize_rep_weights.R
@@ -1,4 +1,5 @@
 suppressPackageStartupMessages(library(survey))
+suppressPackageStartupMessages(library(dplyr))
 
 # Create example data ----
 set.seed(1999)
@@ -10,6 +11,7 @@ cluster_sample[['response_status']] <- sample(x = c('R', 'NR', 'UE', 'IE'),
                                               replace = TRUE)
 cluster_design <- svydesign(id=~dnum, data=cluster_sample, weights = ~ pw)
 cluster_rep_design <- as.svrepdesign(cluster_design, type="JK1")
+num_replicates <- ncol(cluster_rep_design$repweights)
 
 # Test of error messages ----
 test_that("Informative error messages for bad inputs", {
@@ -26,6 +28,14 @@ test_that("Informative error messages for bad inputs", {
   expect_error(
     object = summarize_rep_weights(cluster_rep_design, type = NULL),
     regexp = "must be one of"
+  )
+})
+
+test_that("Throws informative error for missing `by` variables", {
+  expect_error(summarize_rep_weights(
+    cluster_rep_design, type = 'overall', by = c("stype", "is_cromulent")
+  ), regexp = "The following `by` variables are missing from the data.+cromulent",
+  fixed = FALSE
   )
 })
 
@@ -49,5 +59,90 @@ test_that("`type` argument works", {
     object = names(summarize_rep_weights(cluster_rep_design, type = 'specific')),
     expected = c("Rep_Column", "N", "N_NONZERO", "SUM",
                  "MEAN", "CV", "MIN", "MAX")
+  )
+})
+
+test_that("`by` argument works", {
+
+  ## Check that results have expected columns and number of rows
+  grouped_result <- summarize_rep_weights(cluster_rep_design, type = 'both',
+                                          by = c("stype", "sch.wide"))
+
+  expect_equal(
+    object = colnames(grouped_result$overall),
+    expected = c("stype", "sch.wide",
+                 "nrows", "ncols", "degf_svy_pkg", "rank",
+                 "avg_wgt_sum", "sd_wgt_sums",
+                 "min_rep_wgt", "max_rep_wgt")
+  )
+  expect_equal(
+    object = colnames(grouped_result$specific),
+    expected = c("stype", "sch.wide",
+                 "Rep_Column", "N", "N_NONZERO", "SUM",
+                 "MEAN", "CV", "MIN", "MAX")
+  )
+
+  num_groups <- nrow(unique(cluster_rep_design$variables[,c("stype", "sch.wide")]))
+
+  expect_equal(
+    object = nrow(grouped_result$overall),
+    expected = num_groups
+  )
+  expect_equal(
+    object = nrow(grouped_result$specific),
+    expected = num_replicates * num_groups
+  )
+
+  ## Expected values for basic summaries
+  grouped_result <- summarize_rep_weights(cluster_rep_design, type = 'both',
+                                          by = c("sch.wide"))
+
+  expected_summaries <- by(data = weights(cluster_rep_design, type = 'analysis'),
+     INDICES = cluster_rep_design$variables$sch.wide,
+     FUN = function(rep_wts_matrix_subset) {
+       summaries <- list(
+        'matrix_rank' = qr(x = rep_wts_matrix_subset, tol = 1e-05)[['rank']],
+        'nrows' = nrow(rep_wts_matrix_subset),
+        'n_nonzero' = apply(rep_wts_matrix_subset, MARGIN = 2,
+                            FUN = function(wts) sum(wts > 0))
+       )
+     })
+
+  expect_equal(
+    object = grouped_result$overall$rank,
+    expected = c(expected_summaries$No$matrix_rank, expected_summaries$Yes$matrix_rank)
+  )
+
+  expect_equal(
+    object = grouped_result$overall$nrows,
+    expected = c(expected_summaries$No$nrows, expected_summaries$Yes$nrows)
+  )
+
+  expect_equal(
+    object = grouped_result$specific$N_NONZERO |> unname(),
+    expected = c(expected_summaries$No$n_nonzero,
+                 expected_summaries$Yes$n_nonzero) |>
+      unname()
+  )
+
+})
+
+test_that("Works correctly when there are zero rows of data", {
+  expect_equal(
+    object = cluster_rep_design[0,] |>  summarize_rep_weights(type = 'both'),
+    expected = list(
+      overall = structure(list(nrows = 0L, ncols = 15L, degf_svy_pkg = NA_integer_,
+                               rank = NA_integer_, avg_wgt_sum = NA_real_, sd_wgt_sums = NA_real_,
+                               min_rep_wgt = NA_real_, max_rep_wgt = NA_real_),
+                          class = "data.frame", row.names = c(NA, -1L)),
+      specific = structure(list(Rep_Column = 1:15, N = rep(0,times =15),
+                                N_NONZERO = rep(0, times = 15),
+                                SUM = rep(NA_real_, times = 15),
+                                MEAN = rep(NA_real_, times = 15),
+                                CV = rep(NA_real_, times = 15),
+                                MIN = rep(NA_real_, times = 15),
+                                MAX = rep(NA_real_, times = 15)),
+                           class = "data.frame", row.names = c(NA, -15L))
+    )
   )
 })
