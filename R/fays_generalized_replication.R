b18cb28a3757dbccae457957934b5f12ec46e90e
Ben Schneider <Benjamin.Julius.Schneider@gmail.com>
2024-09-12 08:55:13 -0400

    Eigendecomposition is now in a helper function.

--- R/fays_generalized_replication.R
+++ R/fays_generalized_replication.R
@@ -167,45 +167,15 @@
 
 make_fays_gen_rep_factors <- function(
     Sigma,
-    max_replicates = Matrix::rankMatrix(Sigma, method = "qr") + 4,
+    max_replicates = Inf,
     balanced = TRUE
 ) {
 
   n <- nrow(Sigma)
 
   # Calculate spectral decomposition ----
-  svrep_torch_device <- getOption("svrep.torch_device")
+  eigen_decomposition <- compute_eigen_decomposition(Sigma)
   
-  use_torch <- FALSE
-  if (!is.null(svrep_torch_device) && svrep_torch_device %in% c("cpu", "cuda")) {
-    if (requireNamespace("torch", quietly = TRUE)) {
-      use_torch <- torch::torch_is_installed()
-    }
-  }
-  
-  if (!use_torch) {
-    eigen_decomposition <- eigen(x = Sigma, symmetric = TRUE)
-  }
-  
-  if (use_torch) {
-    if (svrep_torch_device == "cuda" && !torch::cuda_is_available()) {
-      svrep_torch_device <- "cpu"
-      message("Using CPU for torch, since `torch::cuda_is_available()` returned FALSE.")
-    }
-    
-    # Note that with 'torch', eigenvalues/vectors are in *ascending* order
-    reversal_indices <- seq(from = ncol(Sigma), to = 1, by = -1)
-    
-    eigen_decomposition <- Sigma |> as.matrix() |> 
-      torch::torch_tensor(device = torch::torch_device(
-        type = svrep_torch_device
-      )) |>
-      torch::linalg_eigh() |>
-      (\(torch_eigh_output) {
-        list('values' = as.numeric(torch_eigh_output[[1]])[reversal_indices],
-             'vectors' = as.matrix(torch_eigh_output[[2]])[,reversal_indices,drop=FALSE])
-      })()
-  }
   Sigma_rank <- Matrix::rankMatrix(Sigma, method = "qr")
 
   # Obtain eigenvectors scaled by square roots of eigenvalues ----
---
+++ R/generalized-replication-helpers.R
@@ -0,0 +1,50 @@
+#' @title Compute the eigendecomposition of a matrix
+#' @details Computes the eigendecomposition of a symmetric matrix,
+#' either using the base R function `eigen()` or instead using the 'torch' 
+#' package if the user has 'torch' installed and
+#' has set `options(svrep.torch_device = "cpu")` 
+#' or `options(svrep.torch_device = "cuda")`.
+#' @param Sigma A symmetric matrix
+#'
+#' @return A list object with two named components:
+#' - `values`: A vector of eigenvalues.
+#' - `vectors`: A matrix with eigenvectors corresponding to the eigenvalues.
+#' @md
+compute_eigen_decomposition <- function(Sigma) {
+  
+  use_torch <- FALSE
+  
+  svrep_torch_device <- getOption("svrep.torch_device")
+  
+  if (!is.null(svrep_torch_device) && svrep_torch_device %in% c("cpu", "cuda")) {
+    if (requireNamespace("torch", quietly = TRUE)) {
+      use_torch <- torch::torch_is_installed()
+    }
+  }
+  
+  if (!use_torch) {
+    eigen_decomposition <- eigen(x = Sigma, symmetric = TRUE)
+  }
+  
+  if (use_torch) {
+    if (svrep_torch_device == "cuda" && !torch::cuda_is_available()) {
+      svrep_torch_device <- "cpu"
+      message("Using CPU for torch, since `torch::cuda_is_available()` returned FALSE.")
+    }
+    
+    # Note that with 'torch', eigenvalues/vectors are in *ascending* order
+    reversal_indices <- seq(from = ncol(Sigma), to = 1, by = -1)
+    
+    eigen_decomposition <- Sigma |> as.matrix() |> 
+      torch::torch_tensor(device = torch::torch_device(
+        type = svrep_torch_device
+      )) |>
+      torch::linalg_eigh() |>
+      (\(torch_eigh_output) {
+        list('values' = as.numeric(torch_eigh_output[[1]])[reversal_indices],
+             'vectors' = as.matrix(torch_eigh_output[[2]])[,reversal_indices,drop=FALSE])
+      })()
+  }
+  
+  return(eigen_decomposition)
+}
