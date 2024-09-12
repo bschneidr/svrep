079eb9b5d2beddee4b81a7b1752b6a58b155d36a
Ben Schneider <Benjamin.Julius.Schneider@gmail.com>
2024-09-12 11:52:22 -0400

    Slight refactoring.

--- R/generalized-replication-helpers.R
+++ R/generalized-replication-helpers.R
@@ -12,6 +12,7 @@
 #' @md
 compute_eigen_decomposition <- function(Sigma) {
   
+  # Determine whether, and how, the torch package should be used
   use_torch <- FALSE
   
   svrep_torch_device <- getOption("svrep.torch_device")
@@ -19,18 +20,21 @@ compute_eigen_decomposition <- function(Sigma) {
   if (!is.null(svrep_torch_device) && svrep_torch_device %in% c("cpu", "cuda")) {
     if (requireNamespace("torch", quietly = TRUE)) {
       use_torch <- torch::torch_is_installed()
+      if (svrep_torch_device == "cuda" && !torch::cuda_is_available()) {
+        svrep_torch_device <- "cpu"
+        message("Using CPU for torch, since `torch::cuda_is_available()` returned FALSE.")
+      }
     }
   }
   
+  
+  # Compute the eigen decomposition with or without torch,
+  # ensure that eigenvalues/vectors are returned in descending order
   if (!use_torch) {
     eigen_decomposition <- eigen(x = Sigma, symmetric = TRUE)
   }
   
   if (use_torch) {
-    if (svrep_torch_device == "cuda" && !torch::cuda_is_available()) {
-      svrep_torch_device <- "cpu"
-      message("Using CPU for torch, since `torch::cuda_is_available()` returned FALSE.")
-    }
     
     # Note that with 'torch', eigenvalues/vectors are in *ascending* order
     reversal_indices <- seq(from = ncol(Sigma), to = 1, by = -1)
